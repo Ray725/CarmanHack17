3d79e120ef0dec0ff8da156009e6afc5cd0567b0
Ray725 <Ray725@users.noreply.github.com>
2017-11-17 00:23:28 -0500

    kinda broken

--- app.py
+++ app.py
@@ -1,24 +1,108 @@
-import tkinter as tk
+from guizero import App, Text, TextBox, PushButton, Combo
+from random import randint
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.pack()
-        self.create_widgets()
 
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+def generateEasy():
+    first = randint(0, 10)
+    second = randint(0, 10)
+    op = randint(0, 1)
+    solution = 0
+    if op == 0:
+        op = "+"
+        solution = first + second
+    else:
+        op = "-"
+        solution = first - second
+    return ["{} {} {}".format(first, op, second), str(solution)]
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=root.destroy)
-        self.quit.pack(side="bottom")
+def generateMedium():
+    first = randint(0, 10)
+    second = randint(0, 10)
+    op = randint(0, 2)
+    solution = 0
+    if op == 0:
+        op = "+"
+        solution = first + second
+    elif op == 1:
+        op = "-"
+        solution = first - second
+    else:
+        op = "x"
+        solution = first * second
+    return ["{} {} {}".format(first, op, second), str(solution)]
 
-    def say_hi(self):
-        print("hi there, everyone!")
+def generateHard():
+    first = random.randint(50, 100)
+    second = random.randint(50, 100)
+    op = randint(0, 3)
+    if op == 0:
+        solution = first + second
+        return ["{} + {}".format(first, second), str(solution)]
+    elif op == 1:
+        solution = first - second
+        return ["{} - {}".format(first, second), str(solution)]
+    elif op == 2:
+        solution = first * second
+        return ["{} x {}".format(first, second), str(solution)]
+    else:
+        op = "/"
+        final = first * second
+        return ["{} / {}".format(final, second), str(first)]
 
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+def submit():
+    if (answer == my_response.get()):
+        text.set("CORRECT")
+        if (difficulty_combo.get() == "Easy"):
+            question = generateEasy()
+            next_ques.set(question[0])
+            answer = question[1]
+        elif (difficulty_combo.get() == "Medium"):
+            question = generateMedium()
+            next_ques.set(question[0])
+            answer = question[1]
+        else:
+            question = generateHard()
+            next_ques.set(question[0])
+            answer = question[1]
+    else:
+        text.set("INCORRECT")
+        if (difficulty_combo.get() == "Easy"):
+            question = generateEasy()
+            next_ques.set(question[0])
+            answer = question[1]
+        elif (difficulty_combo.get() == "Medium"):
+            question = generateMedium()
+            next_ques.set(question[0])
+            answer = question[1]
+        else:
+            question = generateHard()
+            next_ques.set(question[0])
+            answer = question[1]
+
+def start():
+    start.disable()
+    global answer
+    if (difficulty_combo.get() == "Easy"):
+        question = generateEasy()
+        text.set(question[0])
+        answer = question[1]
+    elif (difficulty_combo.get() == "Medium"):
+        question = generateMedium()
+        text.set(question[0])
+        answer = question[1]
+    else:
+        question = generateHard()
+        text.set(question[0])
+        answer = question[1]
+
+# main app loop
+app = App(title = "Math Math Triple Dash")
+
+text = Text(app, text="Welcome to Math Math Triple Dash", size=22, font="Times New Roman", color="blue")
+next_ques = Text(app, text="", size=22, font="Times New Roman", color="blue")
+my_response = TextBox(app)
+start = PushButton(app, command=start, text="Start")
+submit = PushButton(app, command=submit, text="Submit")
+difficulty_combo = Combo(app, options=["Easy", "Medium", "Hard"])
+
+app.display()
