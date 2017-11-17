801adad9409ab47356cb7f0200537a0a2a6eb2db
Ray725 <Ray725@users.noreply.github.com>
2017-11-17 00:38:21 -0500

    finished

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from guizero import App, Text, TextBox, PushButton, Combo
-from random import randint
-
+from random import *
 
 def generateEasy():
     first = randint(0, 10)
     second = randint(0, 10)
     op = randint(0, 1)
-    solution = 0
     if op == 0:
         op = "+"
         solution = first + second
@@ -32,8 +30,8 @@ def generateMedium():
     return ["{} {} {}".format(first, op, second), str(solution)]
 
 def generateHard():
-    first = random.randint(50, 100)
-    second = random.randint(50, 100)
+    first = randint(0, 50)
+    second = randint(1, 50)
     op = randint(0, 3)
     if op == 0:
         solution = first + second
@@ -50,8 +48,14 @@ def generateHard():
         return ["{} / {}".format(final, second), str(first)]
 
 def submit():
+    global answer
+    global score
+    global totalQuestions
+    totalQuestions += 1
     if (answer == my_response.get()):
-        text.set("CORRECT")
+        my_response.clear()
+        score += 1
+        text.set("CORRECT! Score: {}/{}".format(score, totalQuestions))
         if (difficulty_combo.get() == "Easy"):
             question = generateEasy()
             next_ques.set(question[0])
@@ -65,7 +69,8 @@ def submit():
             next_ques.set(question[0])
             answer = question[1]
     else:
-        text.set("INCORRECT")
+        my_response.clear()
+        text.set("INCORRECT. Score: {}/{}".format(score, totalQuestions))
         if (difficulty_combo.get() == "Easy"):
             question = generateEasy()
             next_ques.set(question[0])
@@ -81,6 +86,7 @@ def submit():
 
 def start():
     start.disable()
+    submit.enable()
     global answer
     if (difficulty_combo.get() == "Easy"):
         question = generateEasy()
@@ -95,14 +101,19 @@ def start():
         text.set(question[0])
         answer = question[1]
 
-# main app loop
-app = App(title = "Math Math Triple Dash")
+def change_text_size(slider_value):
+    text.font_size(slider_value)
 
+# main app loop
+totalQuestions = 0
+score = 0
+app = App(title = "Math Math Triple Dash")
 text = Text(app, text="Welcome to Math Math Triple Dash", size=22, font="Times New Roman", color="blue")
 next_ques = Text(app, text="", size=22, font="Times New Roman", color="blue")
 my_response = TextBox(app)
 start = PushButton(app, command=start, text="Start")
 submit = PushButton(app, command=submit, text="Submit")
+submit.disable()
 difficulty_combo = Combo(app, options=["Easy", "Medium", "Hard"])
 
 app.display()
